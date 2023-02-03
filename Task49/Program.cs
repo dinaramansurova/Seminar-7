e2750ca722d9efee19061f2ce2cd8d3b7752b69b
Dinara <ash.solarium@gmail.com>
2023-02-03 11:11:28 +0300

    tasks

---
+++ Task49/Program.cs
@@ -0,0 +1,52 @@
+﻿// Task 49
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса четные, 
+// и замените эти элементы на их квадраты
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+                matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив:");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+PrintMatrix(matrix);
